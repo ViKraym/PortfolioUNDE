0edc9300496eee98fa3e17616eaa548a895a66bf
ViKraym <168685754+ViKraym@users.noreply.github.com>
2025-07-07 18:51:31 +0300

    Update gamesBox.js

--- CodemuLesons/scripts/gamesBox.js
+++ CodemuLesons/scripts/gamesBox.js
@@ -1,9 +1,32 @@
-const myButton = document.getElementsByTagName("button");
-let a = myButton.innerHTML;
-console.log (a);
+const buttons = document.querySelectorAll('button');
+const botNumber = getRandomInt(1, 11);
+// console.log(botNumber);
 
- for (button of myButton) {
-            console.log(button.length);   // выводим текст параграфа
-        }
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// https://metanit.com/web/javascript/8.2.php
+
+ buttons.forEach( function(btn) {
+    
+    btn.addEventListener('click', function(){
+        let b = this.innerHTML;
+          console.log(b);
+          console.log(this);
+
+
+          if (this.innerHTML == Number(botNumber)) {
+            this.innerHTML = "✔"
+          } else {
+            this.innerHTML = "✖"
+          }
+        
+          
+
+
+      }
+
+    );
+ });
