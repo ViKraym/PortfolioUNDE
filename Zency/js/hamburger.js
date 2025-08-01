eeafeb14dd469140cdfee7d9bea93d7dbc8ea05b
ViKraym <viktor.ivan0v98@yandex.ru>
2025-08-01 13:06:58 +0300

    asda

--- Zency/js/hamburger.js
+++ Zency/js/hamburger.js
@@ -1,4 +1,7 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.menu-list').classList.toggle('active');
+
+    // Добавляем/убираем overflow у body при открытии/закрытии меню
+    document.body.classList.toggle('no-scroll');
 });
